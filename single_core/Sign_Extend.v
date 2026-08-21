056ec3eb9187a3b9b88c084006171782668f8ba5
Swastik Aditya Ranjan <123ee0320raj@gmail.com>
2026-08-21 23:01:23 +0530

    fixing error

--- single_core/Sign_Extend.v
+++ single_core/Sign_Extend.v
@@ -1,10 +1,12 @@
-module Sign_Extend(In,Imm_Ext);
+module Sign_Extend(In,ImmSrc,Imm_Ext);
 
     input [31:0] In;
+    input [1:0] ImmSrc;
     output [31:0] Imm_Ext;
 
-    assign Imm_Ext = (ImmSrc == 1'b1) ? {{{20{In[31]}}}, In[31:25], In[11:7]} : 
-                                        {{20{1'b0}}, In[31:20]}; //if In[31] is 1 then Imm_Ext=In[31:20] with 20 1's else Imm_Ext=In[31:20] with 20 0's
+    assign Imm_Ext = (ImmSrc == 2'b01) ? {{20{In[31]}}, In[31:25], In[11:7]} :
+                     (ImmSrc == 2'b10) ? {{19{In[31]}}, In[31], In[7], In[30:25], In[11:8], 1'b0} :
+                                         {{20{In[31]}}, In[31:20]};
 
 
-endmodulecahtsda
+endmodule
